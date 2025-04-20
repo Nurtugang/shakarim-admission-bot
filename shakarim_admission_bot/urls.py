82545cd062b3c8d897100a1a6250efa862705469
Nurtugang <nurtugang17@gmail.com>
2025-04-21 00:42:21 +0500

    Добавлен новый индексный маршрут и шаблон для страницы документации API

--- shakarim_admission_bot/urls.py
+++ shakarim_admission_bot/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
+    path("", include("index.urls")),
 ]
 
 # if settings.DEBUG:
